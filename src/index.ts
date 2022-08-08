4c88fb3fdb3eaa104a090318081df218c8c2de9b
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2022-08-08 09:01:42 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { decrypt, encrypt } from "./modules/crypto";
+export { Route } from "./modules/route";
+export { runBackup } from "./modules/backup";
+export { getBackup } from "./modules/get-backup";
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export {
+  EncryptDataProps,
+  EncryptDataResult,
+  DecryptDataProps,
+} from "./crypto";
+
+export {
+  BackupModelFileEncrypted,
+  BackupModelProps,
+  BackupModels,
+  BackupModelsValue,
+  BackupProps,
+  BackupRecursiveProps,
+} from "./backup";
